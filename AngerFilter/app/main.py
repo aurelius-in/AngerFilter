96844debf35aea8c26a0a84dcb1cc57dbd9a72a6
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-04-15 23:17:43 -0500

    Update main.py

--- AngerFilter/app/main.py
+++ AngerFilter/app/main.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template, request
 import gpt3_api_client
 
+# This is a Python Flask web application that 
+# uses the OpenAI GPT-3 API to generate 
+# alternative messages based on # user input. 
+# The application has four routes:
 
+# '/' : This route returns the index.html template, 
+# which contains a form for the user to enter input.
+
+# '/generate': This route is called when the form is 
+# submitted. It extracts the user input from the form 
+# and passes it to the GPT-3 API client to generate 
+# alternative messages. It then returns the alt_messages.html 
+# template, which displays the generated messages.
+# alt_messages.html template, which displays the generated messages.
+
+# '/settings': This route returns the settings.html template, which 
+# could contain options for the user to customize the GPT-3 API client's behavior.
+
+# '/about': This route returns the about.html template, which could 
+# contain information about the application and its developers.
+
+# The GPT-3 API client is initialized outside of any route to ensure 
+# that it is only initialized once when the application starts.
+# This avoids unnecessary API calls and improves the application's performance. 
+# The debug flag is set to True in the app.run() method to enable debug mode.
+# This should be turned off in production to avoid security risks.
 
 app = Flask(__name__)
 
